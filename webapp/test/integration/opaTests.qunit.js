13d909d6672790c625493e02d47ea1f2e8d36711
Tyron Jones <tyron.jones@tempioneer.com>
2024-10-01 14:03:38 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["student2912/com/sap/training/ux402/repository/ux402repository/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"student2912comsaptrainingux402repository/ux402_repository/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
